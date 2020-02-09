53f6aeb928b995625a0c471656bb3648d1e9b310
jaymp-glitch <jayasree.prakasan1@outlook.com>
2020-02-09 11:48:13 +0530

    EntityyFramework mapped to DB ITY502Db and table LoginDetails

---
+++ ITY502/DBScripts/InsertLoginDetails.sql
@@ -0,0 +1,16 @@
+insert into LoginDetails values(
+'TestAdmin',
+'TestAdmin',
+'Admin'
+)
+insert into LoginDetails values(
+'TestPublic',
+'TestPublic',
+'Public'
+)
+insert into LoginDetails values(
+'TestEmployee',
+'TestEmployee',
+'Employee'
+)
+select * from LoginDetails
