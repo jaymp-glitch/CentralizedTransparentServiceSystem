ac41c735b8871bcba29fa8977762a19b49a09e93
jaymp-glitch <jayasree.prakasan1@outlook.com>
2020-02-11 08:21:48 +0530

    logout,session expirinfg logic added

--- ITY502/Controllers/AdminController.cs
+++ ITY502/Controllers/AdminController.cs
@@ -3,12 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Routing;
 namespace ITY502.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+
+        {
+
+            HttpSessionStateBase session = filterContext.HttpContext.Session;
+
+            Controller controller = filterContext.Controller as Controller;
+
+
+            if (session == null)
+            {
+                ViewBag.updateAccountMessage = "Session Expired";
+
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+
+                {
+
+                    controller = "Home",
+
+                    action = "Login"
+
+                }));
+            }
+            else if (session != null && (session["UserType"].ToString()) != "Admin")
+
+            {
+
+                ViewBag.updateAccountMessage = "Session Expired";
+
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+
+                {
+
+                    controller = "Home",
+
+                    action = "Login"
+
+                }));
+
+            }
+
+            base.OnActionExecuting(filterContext);
+
+        }
         public ActionResult AdminHomePage()
         {
             return View();
--- ITY502/Controllers/PublicController.cs
+++ ITY502/Controllers/PublicController.cs
@@ -3,12 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace ITY502.Controllers
 {
     public class PublicController : Controller
     {
         // GET: Public
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+
+        {
+
+            HttpSessionStateBase session = filterContext.HttpContext.Session;
+
+            Controller controller = filterContext.Controller as Controller;
+
+
+            if (session == null)
+            {
+                ViewBag.updateAccountMessage = "Session Expired";
+
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+
+                {
+
+                    controller = "Home",
+
+                    action = "Login"
+
+                }));
+            }
+            else if (session != null && (session["UserType"].ToString()) != "Public")
+
+            {
+
+                ViewBag.updateAccountMessage = "Session Expired";
+
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+
+                {
+
+                    controller = "Home",
+
+                    action = "Login"
+
+                }));
+
+            }
+
+            base.OnActionExecuting(filterContext);
+
+        }
         public ActionResult PublicHomePage()
         {
             return View();
